8578e4954364887b8ee8886cd7ffe627e2d33984
Andy Dayton <amdayton@gmail.com>
2016-04-19 10:06:36 -0400

    Basic mapping is working! Ready to inc video

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -13,87 +13,83 @@ void ofApp::setup(){
 	// but we have to explicitly normalize our tex coords here
 	ofEnableNormalizedTexCoords();
 
-  ofEnableArbTex();
-
   gui.setup();
-  gui.add(showGrid.setup("Show grid", true));
+  gui.add(showGrid.setup("Show grid", false));
   gui.add(drawWireframe.setup("Draw wireframe", false));
-//  gui.add(cornerScale.setup("Corner scale", 100, 0, 1000));
-
+  gui.add(tri1Tx0s.setup("tri1Tx0s", 0.5, 0, 1));
+  gui.add(tri1Tx0t.setup("tri1Tx0t", 0.5, 0, 1));
+  gui.add(tri1Tx1s.setup("tri1Tx1s", 1, 0, 1));
+  gui.add(tri1Tx1t.setup("tri1Tx1t", 0.2, 0, 1));
+  gui.add(tri1Tx2s.setup("tri1Tx2s", 0, 0, 1));
+  gui.add(tri1Tx2t.setup("tri1Tx2t", 0.2, 0, 1));
+  
+  gui.add(tri2Tx0s.setup("tri2Tx0s", 0.5, 0, 1));
+  gui.add(tri2Tx0t.setup("tri2Tx0t", 0.5, 0, 1));
+  gui.add(tri2Tx1s.setup("tri2Tx1s", 0, 0, 1));
+  gui.add(tri2Tx1t.setup("tri2Tx1t", 1, 0, 1));
+  gui.add(tri2Tx2s.setup("tri2Tx2s", 0, 0, 1));
+  gui.add(tri2Tx2t.setup("tri2Tx2t", 0.2, 0, 1));
+  
+  gui.add(tri3Tx0s.setup("tri3Tx0s", 0.5, 0, 1));
+  gui.add(tri3Tx0t.setup("tri3Tx0t", 0.5, 0, 1));
+  gui.add(tri3Tx1s.setup("tri3Tx1s", 0.5, 0, 1));
+  gui.add(tri3Tx1t.setup("tri3Tx1t", 1, 0, 1));
+  gui.add(tri3Tx2s.setup("tri3Tx2s", 1, 0, 1));
+  gui.add(tri3Tx2t.setup("tri3Tx2t", 0.2, 0, 1));
+  
   image.load("corner-1024.jpg");
-//  imageTexture.allocate(image.getPixels(), true);
-//  ofLoadImage(imageTexture, "corner-1024.jpg");
   
-  corner.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+  tri1.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+  tri1.addVertex(ofPoint(0, 0, 0));
+  tri1.addVertex(ofPoint(cornerScale, 0, 0));
+  tri1.addVertex(ofPoint(0, -cornerScale, 0));
+
+  tri1.addIndex(0);
+  tri1.addIndex(1);
+  tri1.addIndex(2);
+
+  tri2.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+  tri2.addVertex(ofPoint(0, 0, 0));
+  tri2.addVertex(ofPoint(0, 0, cornerScale));
+  tri2.addVertex(ofPoint(0, -cornerScale, 0));
+
+  tri2.addIndex(0);
+  tri2.addIndex(1);
+  tri2.addIndex(2);
   
-  corner.addVertex(ofPoint(0, 0, 0));
-  corner.addVertex(ofPoint(cornerScale, 0, 0));
-  corner.addVertex(ofPoint(cornerScale, -cornerScale, 0));
-  corner.addVertex(ofPoint(0, -cornerScale, 0));
+  tri3.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
+  tri3.addVertex(ofPoint(0, 0, 0));
+  tri3.addVertex(ofPoint(cornerScale, 0, 0));
+  tri3.addVertex(ofPoint(0, 0, cornerScale));
+
+  tri3.addIndex(0);
+  tri3.addIndex(1);
+  tri3.addIndex(2);
   
-  corner.addVertex(ofPoint(0, 0, 0));
-  corner.addVertex(ofPoint(0, 0, cornerScale));
-  corner.addVertex(ofPoint(0, -cornerScale, cornerScale));
-  corner.addVertex(ofPoint(0, -cornerScale, 0));
-
-  corner.addVertex(ofPoint(0, 0, 0));
-  corner.addVertex(ofPoint(cornerScale, 0, 0));
-  corner.addVertex(ofPoint(cornerScale, 0, cornerScale));
-  corner.addVertex(ofPoint(0, 0, cornerScale));
-
-  corner.addVertex(ofPoint(0, 0, 0));
-
-
-  corner.addIndex(5);
-  corner.addIndex(1);
-  corner.addIndex(3);
-  corner.addIndex(6);
-//  corner.addIndex(0);
-
-  corner.addTexCoord(ofVec2f(0,0));
-  corner.addTexCoord(ofVec2f(0, 1));
-  corner.addTexCoord(ofVec2f(1, 1));
-  corner.addTexCoord(ofVec2f(1, 0));
-//  corner.addTexCoord(ofVec2f(0.5, 0.5));
-  
-//  rect.setMode(OF_PRIMITIVE_TRIANGLE_FAN);
-//  rect.addVertex(ofPoint(0, 0, 0));
-//  rect.addVertex(ofPoint(cornerScale, 0, 0));
-//  rect.addVertex(ofPoint(cornerScale, -cornerScale, 0));
-//  rect.addVertex(ofPoint(0, -cornerScale, 0));
-//  rect.addVertex(ofPoint(0, 0, 0));
-//  
-//  
-//  rect.addIndex(0);
-//  rect.addIndex(1);
-//  rect.addIndex(2);
-//  rect.addIndex(3);
-//
-//  rect.addTexCoord(ofVec2f(0, 1));
-//  rect.addTexCoord(ofVec2f(1, 1));
-//  rect.addTexCoord(ofVec2f(1, 0));
-//  rect.addTexCoord(ofVec2f(0, 0));
-//  
-//  points[0].x = 100;
-//  points[0].y = 100;
-//  points[1].x = 600;
-//  points[1].y = 100;
-//  points[2].x = 600;
-//  points[2].y = 600;
-//  points[3].x = 100;
-//  points[3].y = 600;
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+  tri1.clearTexCoords();
+  tri1.addTexCoord(ofVec2f(tri1Tx0s, tri1Tx0t));
+  tri1.addTexCoord(ofVec2f(tri1Tx1s, tri1Tx1t));
+  tri1.addTexCoord(ofVec2f(tri1Tx2s, tri1Tx2t));
+  
+  
+  tri2.clearTexCoords();
+  tri2.addTexCoord(ofVec2f(tri2Tx0s, tri2Tx0t));
+  tri2.addTexCoord(ofVec2f(tri2Tx1s, tri2Tx1t));
+  tri2.addTexCoord(ofVec2f(tri2Tx2s, tri2Tx2t));
+  
+  tri3.clearTexCoords();
+  tri3.addTexCoord(ofVec2f(tri3Tx0s, tri3Tx0t));
+  tri3.addTexCoord(ofVec2f(tri3Tx1s, tri3Tx1t));
+  tri3.addTexCoord(ofVec2f(tri3Tx2s, tri3Tx2t));
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-//  ofClear(0);
-
-//  imageTexture.draw(points[0], points[1], points[2], points[3]);
+  ofClear(0);
 
   cam.begin();
   
@@ -101,39 +97,38 @@ void ofApp::draw(){
     if(showGrid) {
       ofDrawGrid(10);
     }
-    image.getTexture().bind();
   
       if(drawWireframe) {
-        corner.drawWireframe();
-//        rect.drawWireframe();
+        tri1.drawWireframe();
+        tri2.drawWireframe();
+        tri3.drawWireframe();
       } else {
-        corner.draw();
-//        rect.draw();
+        image.getTexture().bind();
+        tri1.draw();
+        tri2.draw();
+        tri3.draw();
+        image.getTexture().unbind();
+
       }
-    image.getTexture().unbind();
   cam.end();
   
   ofDisableDepthTest();
   
-//  ofDisableLighting();
-  gui.draw();
+  if(showGui) {
+    gui.draw();
+  }
 
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-//  if(key == '1') {
-//    cornerIndex = 0;
-//  }
-//  if(key == '2') {
-//    cornerIndex = 1;
-//  }
-//  if(key == '3') {
-//    cornerIndex = 2;
-//  }
-//  if(key == '4') {
-//    cornerIndex = 3;
-//  }
+  if(key == 'g') {
+    showGui = !showGui;
+  }
+  
+  if(key == 'f') {
+    ofToggleFullscreen();
+  }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -21,6 +21,8 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+    bool showGui = true;
   
     ofImage image;
     ofTexture imageTexture;
@@ -29,12 +31,35 @@ class ofApp : public ofBaseApp{
 		
     ofLight light;
     ofEasyCam cam;
-    ofMesh corner;
-    ofMesh rect;
+    ofMesh tri1;
+    ofMesh tri2;
+    ofMesh tri3;
   
   
     ofxToggle showGrid;
     ofxToggle drawWireframe;
+  
+    ofxFloatSlider tri1Tx0s;
+    ofxFloatSlider tri1Tx0t;
+    ofxFloatSlider tri1Tx1s;
+    ofxFloatSlider tri1Tx1t;
+    ofxFloatSlider tri1Tx2s;
+    ofxFloatSlider tri1Tx2t;
+
+    ofxFloatSlider tri2Tx0s;
+    ofxFloatSlider tri2Tx0t;
+    ofxFloatSlider tri2Tx1s;
+    ofxFloatSlider tri2Tx1t;
+    ofxFloatSlider tri2Tx2s;
+    ofxFloatSlider tri2Tx2t;
+
+    ofxFloatSlider tri3Tx0s;
+    ofxFloatSlider tri3Tx0t;
+    ofxFloatSlider tri3Tx1s;
+    ofxFloatSlider tri3Tx1t;
+    ofxFloatSlider tri3Tx2s;
+    ofxFloatSlider tri3Tx2t;
+  
 //    ofxFloatSlider cornerScale;
     float cornerScale = 100;
     ofxPanel gui;
